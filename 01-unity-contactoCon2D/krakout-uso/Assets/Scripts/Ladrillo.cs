825ad439c17a7aa9e50ae9d64f84ed40ce0a30f7
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-09-18 11:17:16 +0200

    Krakout, versión 0.04 : Prefabs, ladrillos, material físico

--- 01-unity-contactoCon2D/krakout-uso/Assets/Scripts/Ladrillo.cs
+++ 01-unity-contactoCon2D/krakout-uso/Assets/Scripts/Ladrillo.cs
@@ -2,19 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pelota : MonoBehaviour
+public class Ladrillo : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody2D>().
-            velocity = new Vector2(3, 2);
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Boom");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
     }
 }
