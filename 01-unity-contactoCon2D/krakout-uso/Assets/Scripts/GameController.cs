52363e9c6538586990c2cb2212000752ca0d2c13
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-09-25 16:56:23 +0200

    Krakout, versión 0.11 : Dos niveles enlazados

--- 01-unity-contactoCon2D/krakout-uso/Assets/Scripts/GameController.cs
+++ 01-unity-contactoCon2D/krakout-uso/Assets/Scripts/GameController.cs
@@ -1,15 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-    private int puntos = 0;
+    static private int puntos = 0;
+    //static private int vidas = 3;
+    [SerializeField] int nivel = 1;
+    [SerializeField] int nivelMax = 2;
+    [SerializeField] int ladrillos = 3;
     [SerializeField] UnityEngine.UI.Text textoPuntos;
+
+    void Start()
+    {
+        ActualizarMarcador();
+    }
 
     public void IncrementarPuntos()
     {
         puntos += 10;
-        textoPuntos.text = "Puntos\n" + puntos;
+        ladrillos--;
+        ActualizarMarcador();
+
+        if (ladrillos <= 0)
+        {
+            if (nivel < nivelMax)
+            {
+                nivel++;
+                SceneManager.LoadScene("Nivel"
+                    + nivel.ToString("00"));
+            }
+            else
+            {
+                puntos = 0;
+                nivel = 1;
+                SceneManager.LoadScene("Menu");
+            }
+        }
+    }
+
+    private void ActualizarMarcador()
+    {
+        textoPuntos.text = "Puntos\n" + puntos
+             + "\n\nNivel\n" + nivel
+             + "\n\nRestantes\n" + ladrillos;
     }
 }
