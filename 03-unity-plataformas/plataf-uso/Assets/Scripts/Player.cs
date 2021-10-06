298cc06d7613ec1edd7acb22ba7f45ee9a3bc7cf
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-10-06 18:04:34 +0200

    Plataformas 0.02: Sin salto múltiple (raycast)

--- 03-unity-plataformas/plataf-uso/Assets/Scripts/Player.cs
+++ 03-unity-plataformas/plataf-uso/Assets/Scripts/Player.cs
@@ -6,11 +6,13 @@ public class Player : MonoBehaviour
 {
     float velocidad = 10f;
     float fuerzaSalto = 5f;
+    float alturaPersonaje;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        alturaPersonaje = GetComponent<Collider2D>().
+            bounds.size.y;
     }
 
     // Update is called once per frame
@@ -24,9 +26,21 @@ public class Player : MonoBehaviour
             );
         if (Input.GetButtonDown("Jump"))
         {
-            GetComponent<Rigidbody2D>().
-                AddForce(Vector2.up * fuerzaSalto, 
-                ForceMode2D.Impulse);
+            RaycastHit2D hit = Physics2D.Raycast(
+                transform.position, Vector2.down);
+            if (hit.collider != null)
+            {
+                float distanciaAlSuelo = hit.distance;
+                bool tocandoSuelo = distanciaAlSuelo < alturaPersonaje;
+                if (tocandoSuelo)
+                {
+                    GetComponent<Rigidbody2D>().
+                        AddForce(Vector2.up * fuerzaSalto,
+                        ForceMode2D.Impulse);
+                }
+
+            }
+
         }
     }
 }
