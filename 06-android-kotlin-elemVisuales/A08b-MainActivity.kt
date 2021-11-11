780a73ca398d2ca6fcca5d22b7b38342793716ad
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-11-11 23:48:57 +0100

    Android 08: ListView, primer contacto

---
+++ 06-android-kotlin-elemVisuales/A08b-MainActivity.kt
@@ -0,0 +1,26 @@
+package com.nachocabanes.listview1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
+class MainActivity : AppCompatActivity() {
+
+    val datos = arrayOf("Uno", "Dos", "Tres", "Cuatro",
+        "Cinco", "Seis", "Siete", "Ocho",
+        "Nueve", "Diez", "Once", "Doce",
+        "Trece", "Catorce")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val adaptador = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1,
+            datos)
+
+        val lvEjemplo : ListView = findViewById(R.id.lvEjemplo)
+        lvEjemplo.adapter = adaptador
+    }
+}
