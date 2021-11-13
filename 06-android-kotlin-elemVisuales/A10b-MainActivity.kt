33a917d65cb883e338cb4052aec704728496471a
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-11-13 12:32:19 +0100

    Android 10: ListView, onClick

---
+++ 06-android-kotlin-elemVisuales/A10b-MainActivity.kt
@@ -0,0 +1,50 @@
+package com.nachocabanes.listview1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    /*
+    val datos = arrayOf("Uno", "Dos", "Tres", "Cuatro",
+        "Cinco", "Seis", "Siete", "Ocho",
+        "Nueve", "Diez", "Once", "Doce",
+        "Trece", "Catorce")
+     */
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val datos = resources.getStringArray(R.array.datosLista)
+
+        val adaptador = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1,
+            datos)
+
+        val lvEjemplo : ListView = findViewById(R.id.lvEjemplo)
+        lvEjemplo.adapter = adaptador
+
+        lvEjemplo.onItemClickListener =
+            object : AdapterView.OnItemClickListener {
+                override fun onItemClick(
+                        parent: AdapterView<*>?,
+                        view: View?,
+                        pos: Int,
+                        id: Long)
+                {
+                    Toast.makeText(
+                        applicationContext,
+                        "Escogido: ${datos[pos]}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+    }
+}
