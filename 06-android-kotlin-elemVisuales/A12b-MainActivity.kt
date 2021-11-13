8e3deead4adc03745ecf1cf714d50b134ed0060a
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-11-13 12:35:08 +0100

    Android 12: ListView, elemento con layout XML

---
+++ 06-android-kotlin-elemVisuales/A12b-MainActivity.kt
@@ -0,0 +1,63 @@
+package com.nachocabanes.listview1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+
+    /*
+    val datos = arrayOf("Uno", "Dos", "Tres", "Cuatro",
+        "Cinco", "Seis", "Siete", "Ocho",
+        "Nueve", "Diez", "Once", "Doce",
+        "Trece", "Catorce")
+     */
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val datos = resources.getStringArray(R.array.datosLista)
+
+        val adaptador = ArrayAdapter(this,
+            // android.R.layout.simple_list_item_1,
+            R.layout.elemento_de_lista,
+            datos)
+
+        val lvEjemplo : ListView = findViewById(R.id.lvEjemplo)
+        lvEjemplo.adapter = adaptador
+
+        lvEjemplo.onItemClickListener =
+            object : AdapterView.OnItemClickListener {
+                override fun onItemClick(
+                        parent: AdapterView<*>?,
+                        view: View?,
+                        pos: Int,
+                        id: Long)
+                {
+                    Snackbar.make(
+                        view!!,
+                        "Escogido: ${datos[pos]}",
+                        Snackbar.LENGTH_INDEFINITE
+                    ).setAction(
+                        "Entendido"
+                    ) {
+                        // Bla bla bla
+                    }.show()
+                        /*
+                        Toast.makeText(
+                            applicationContext,
+                            "Escogido: ${datos[pos]}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                         */
+                }
+            }
+
+    }
+}
