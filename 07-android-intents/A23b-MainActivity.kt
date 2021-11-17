336bf83afd8f2fb0081962158906ecdcdba4c1c2
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-11-17 20:24:07 +0100

    Android 023: Intents 01, segunda activity

---
+++ 07-android-intents/A23b-MainActivity.kt
@@ -0,0 +1,20 @@
+package com.nachocabanes.intents01
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btSaludar.setOnClickListener {
+            val miIntent = Intent(this,
+                ActivitySaludo::class.java)
+            startActivity(miIntent)
+
+        }
+    }
+}
