1bc40137f3bb93a136d6fba1a890948ba5ca45dd
Nacho Cabanes <ncabanes@users.noreply.github.com>
2021-12-13 23:35:26 +0100

    Android 045: Notificaciones + activity secundaria

---
+++ 08-android-dialog-async-notif/A45c-ActivityAviso.kt
@@ -0,0 +1,11 @@
+package com.nachocabanes.notifs01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityAviso : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aviso)
+    }
+}
